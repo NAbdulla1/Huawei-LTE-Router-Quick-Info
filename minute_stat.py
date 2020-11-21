be2287a24d5465291ff118cc1c628077b5412c4c
Md. Abdulla Al Mamun Nayon <nayonabdulla@gmail.com>
2020-11-21 20:33:03 +0600

    change statistics strategy and add sync lock to avoid race condition

--- minute_stat.py
+++ minute_stat.py
@@ -1,35 +1,52 @@
 from datetime import datetime, timedelta
+from threading import Lock
 import time
 import math
 import util
 
 
 class LastKMinuteStat:
-    def __init__(self, minute):
-        self.k = timedelta(seconds=minute*60)  # converting to seconds
+    def __init__(self, k_minute):
+        self.k = timedelta(seconds=k_minute*60)  # converting to seconds
         self.list = []
+        self.last = -1
+        self.sum = 0
+        self.lock = Lock()
 
     def append(self, up, down):
         cur = up+down
-        self.list.append((datetime.now(), cur))
+        to_add = 0
+        if self.last != -1:
+            to_add = cur-self.last
+        self.last = cur
+        self.list.append((datetime.now(), to_add))
+        self.sum += to_add
+        self.removeOlderStat()
+
+    def removeOlderStat(self):
         now = datetime.now()
         while len(self.list) > 0 and (now-self.list[0][0]) > self.k:
+            self.sum -= self.list[0][1]
             self.list.pop(0)
+        if len(self.list) == 0:
+            self.sum = 0
+            self.last = -1
 
     def getStat(self):
-        sum = 0
         perSec = 0
-        now = datetime.now()
-        while len(self.list) > 0 and (now-self.list[0][0]) > self.k:
-            self.list.pop(0)
+        self.removeOlderStat()
         if len(self.list) > 0:
-            sum = math.fabs(self.list[-1][1]-self.list[0][1])
-            perSec = sum/self.k.seconds
-        return (util.data_unit_calc(sum, "B"), util.data_unit_calc(perSec, "Bps"))
+            perSec = self.sum/self.k.seconds
+        return (util.data_unit_calc(self.sum, "B"), util.data_unit_calc(perSec, "Bps"))
 
     def getFormattedStat(self):
-        (tot, totps) = self.getStat()
-        return "%s\n%s" % (tot, totps)
+        with self.lock:
+            (tot, totps) = self.getStat()
+            return "%s\n%s" % (tot, totps)
 
     def clear(self):
-        self.list.clear()
+        with self.lock:
+            print("cleared")
+            self.list.clear()
+            self.sum = 0
+            self.last = -1
