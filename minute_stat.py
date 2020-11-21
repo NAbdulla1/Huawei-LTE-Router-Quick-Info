e567f99c26b3353e2e063036d784f6ebfb347e38
Md. Abdulla Al Mamun Nayon <nayonabdulla@gmail.com>
2020-11-21 15:25:27 +0600

    changed append and get statistics strategy

--- minute_stat.py
+++ minute_stat.py
@@ -9,19 +9,20 @@ class LastKMinuteStat:
         self.list = []
 
     def append(self, up, down):
-        self.list.append((datetime.now(), up+down))
-        while len(self.list) > 1000:
+        cur = up+down
+        self.list.append((datetime.now(), cur))
+        now = datetime.now()
+        while len(self.list) > 0 and (now-self.list[0][0]) > self.k:
             self.list.pop(0)
 
     def getStat(self):
-        now = datetime.now()
         sum = 0
         perSec = 0
+        now = datetime.now()
         while len(self.list) > 0 and (now-self.list[0][0]) > self.k:
             self.list.pop(0)
-        l = len(self.list)
-        if l > 0:
-            sum = math.fabs(self.list[l-1][1]-self.list[0][1])
+        if len(self.list) > 0:
+            sum = math.fabs(self.list[-1][1]-self.list[0][1])
             perSec = sum/self.k.seconds
         return (self._data_unit_calc(sum, "B"), self._data_unit_calc(perSec, "Bps"))
 
@@ -36,3 +37,6 @@ class LastKMinuteStat:
             bytes /= 1024
             unitInd += 1
         return ("%6.2f %s%s") % (bytes, units[unitInd], unit)
+
+    def clear(self):
+        self.list.clear()
